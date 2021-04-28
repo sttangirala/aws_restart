a7d2d0552a270370bce463edfe8c6b5380527d43
sttangirala <57788509+sttangirala@users.noreply.github.com>
2021-04-28 08:11:02 -0700

    Add files via upload

---
+++ random_number_game.py
@@ -0,0 +1,18 @@
+print ("Welcome to Guess The Number!") 
+print ("The rules are simple. I will think of a number and you try to guess it.") 
+
+import random 
+
+number = random.randint(1,10) 
+
+isGuessRight = False 
+
+while isGuessRight != True: 
+    guess = input("Guess a number between 1 and 10: ") 
+    if int(guess) == number: 
+        print ("You guessed {}. That is right! You win!".format(guess))
+        isGuessRight = True 
+        
+    else: 
+        print("You guesses {}. Sorry, that isn't it. Try again.".format(guess))
+        
